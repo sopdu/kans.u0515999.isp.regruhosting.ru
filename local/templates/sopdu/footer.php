0f2437f2e13f1b376218acb08032baf16215ad56
alexey <av@inalytics.ru>
2020-08-18 11:31:42 +0300

    Меню в футоре

--- local/templates/sopdu/footer.php
+++ local/templates/sopdu/footer.php
@@ -11,11 +11,28 @@
 			<div class="row">
 				<div class="foot_item col fixmorefooter">
 					<div class="footitem_title"><?=GetMessage("sopduFoorerStill")?></div>
+					<?$APPLICATION->IncludeComponent("bitrix:menu", "footer", Array(
+						"COMPONENT_TEMPLATE" => ".default",
+						"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+						"MENU_CACHE_TYPE" => "N",	// Тип кеширования
+						"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+						"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+						"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+						"MAX_LEVEL" => "1",	// Уровень вложенности меню
+						"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+						"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+						"DELAY" => "N",	// Откладывать выполнение шаблона меню
+						"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+					),
+						false
+					);?>
+					<?/*
 					<div class="footitem_text"><a href="#">О компании</a></div>
 					<div class="footitem_text"><a href="#">Новости</a></div>
 					<div class="footitem_text"><a href="#">Технологии</a></div>
 					<div class="footitem_text"><a href="#">Партнеры</a></div>
 					<div class="footitem_text"><a href="#">Контакты</a></div>
+					*/?>
 				</div>
 				<div class="foot_item col">
 					<div class="footitem_title"><?=GetMessage("sopduFooterCatalog")?></div>
